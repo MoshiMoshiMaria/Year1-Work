b9a4de6d82d8c3557e5bd5d99773c5bba0957379
Daniel Clipston <daniel_clipston@hotmail.com>
2019-11-26 12:32:23 +0000

    Week 2.5

---
+++ Week 2 - Variable Size Display/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "The size of type int is " << sizeof(int) << " bytes." << endl;
+	cout << "The size of type short is " << sizeof(short) << " bytes." << endl;
+	cout << "The size of type long is " << sizeof(long) << " bytes." << endl;
+	cout << "The size of type float is " << sizeof(float) << " bytes." << endl;
+	cout << "The size of type double is " << sizeof(char) << " bytes." << endl;
+	cout << "The size of type bool is " << sizeof(bool) << " bytes." << endl;
+	cout << "The size of type string is " << sizeof(string) << " bytes." << endl;
+	system("pause");
+}
